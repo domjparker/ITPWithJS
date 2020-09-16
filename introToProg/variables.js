19c006be71be810213277eca95c0b7a24617a177
Rees Dart <thedominicparker@gmail.com>
2020-09-16 16:10:02 -0700

    variables chapter added

---
+++ introToProg/variables.js
@@ -0,0 +1,28 @@
+1.
+// let personGreeted = 'Victor';
+
+// console.log(`Good Morning, ${personGreeted}.`)
+// console.log(`Good Afternoon, ${personGreeted}.`)
+// console.log(`Good Evening, ${personGreeted}.`)
+
+2.
+// let age = 3;
+// let ten = 10;
+
+// console.log(`In ${ten} years, my son will be ${ten + age} years old.`)
+// console.log(`In ${ten * 2} years, my son will be ${(ten * 2)+ age} years old.`)
+// console.log(`In ${ten * 3} years, my son will be ${(ten * 3)+ age} years old.`)
+// console.log(`In ${ten * 4} years, my son will be ${(ten * 4)+ age} years old.`)
+
+3.
+// the let variable was created within a block, and therefore is block scoped.
+// When we attempt to console.log it, the it produces an error because the let variable created doesn't exist outside of the block
+
+4.
+// The first 3 console.logs take place, however line 6 produces an error because you can't reassign a value to a const variable.
+
+5.
+// This logs 'qux' to the console because although foo is reassigned within a block, because it was first initiated outside of the block it can therefore be accessed outside of the block with the reassignment still valid.
+
+6.
+// It won't produce an error, but the const declared in the block will have no effect on changing or reassigning the const outside the block because it is a const variable, not a let variable.
