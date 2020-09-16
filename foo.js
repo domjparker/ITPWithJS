159f5dfe5973ccc5f5caf3d01d2bcbe0146c8c7d
Rees Dart <thedominicparker@gmail.com>
2020-09-16 09:22:39 -0700

    preparations

---
+++ foo.js
@@ -0,0 +1,78 @@
+// 3. 
+let foo = 'bar';
+console.log(foo);
+foo;
+
+// 4.
+
+// bar
+
+// Welcome to Node.js v12.16.3.
+// Type ".help" for more information.
+// > let foo = 'bar';
+// undefined
+// > console.log(foo);
+// bar
+// undefined
+// > foo;
+
+// bar
+// "bar"
+
+
+// 5.
+// Identify the Constructors for each of the following methods and classify each method as either a "Static" or a "Prototype" method:
+// substring = prototype > String, 
+// create = static > tabs, object, windows, alarms, bookmarks, notifications, menus
+// fromCharCode = static > String 
+// slice = prototype > ArrayBuffer, Blob, String, SharedArrayBuffer, Array, TypedArray
+
+
+// 6.
+// Which of the following names satisfy the style guidelines for variable names? For the purposes of this question, constants are not variables.
+// index = yes
+// CatName = no
+// snake_case = no
+// lazyDog = yes
+// quick_Fox = no
+// 1stCharacter = no
+// operand2 = yes
+// BIG_NUMBER = no
+// Solution = no
+
+
+// 7.
+// Which of the following names satisfy the style guidelines for function names?
+// index = yes
+// CatName = yes
+// snake_case = no
+// lazyDog = yes
+// quick_Fox = no
+// 1stCharacter = no
+// operand2 = yes
+// BIG_NUMBER = no
+// Solution = no
+
+
+// 8.
+// Which of the following names satisfy the style guidelines for constants?
+// index = no
+// CatName = no
+// snake_case = no
+// lazyDog = no
+// quick_Fox = no
+// 1stCharacter = no
+// operand2 = no
+// BIG_NUMBER = yes
+// Solution = no
+
+// 9.
+// Which of the following names don't satisfy the style guidelines for naming variables, functions, or constants?
+// index
+// CatName
+// snake_case = *
+// lazyDog
+// quick_Fox  = *
+// 1stCharacter = *
+// operand2
+// BIG_NUMBER
